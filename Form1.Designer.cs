2bbbf7dfcd76e82f091d1e48fc78a88c0150f421
107131n <ddolgi1206@icloud.com>
2023-09-08 17:36:11 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace 숫자_맞히기
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button btnOk;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRetry = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            btnOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            btnOk.Cursor = System.Windows.Forms.Cursors.Default;
+            btnOk.Font = new System.Drawing.Font("한컴 윤고딕 250", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            btnOk.Location = new System.Drawing.Point(334, 127);
+            btnOk.Margin = new System.Windows.Forms.Padding(4);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new System.Drawing.Size(85, 40);
+            btnOk.TabIndex = 2;
+            btnOk.Text = "확인";
+            btnOk.UseVisualStyleBackColor = false;
+            btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lbl.Font = new System.Drawing.Font("한컴 윤고딕 250", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbl.Location = new System.Drawing.Point(58, 86);
+            this.lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 23);
+            this.lbl.TabIndex = 0;
+            // 
+            // txt
+            // 
+            this.txt.Font = new System.Drawing.Font("한컴 윤고딕 250", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt.Location = new System.Drawing.Point(57, 138);
+            this.txt.Margin = new System.Windows.Forms.Padding(4);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(245, 29);
+            this.txt.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnStart.Font = new System.Drawing.Font("한컴 윤고딕 250", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(191, 190);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(111, 48);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRetry
+            // 
+            this.btnRetry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnRetry.Font = new System.Drawing.Font("한컴 윤고딕 250", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRetry.Location = new System.Drawing.Point(57, 190);
+            this.btnRetry.Name = "btnRetry";
+            this.btnRetry.Size = new System.Drawing.Size(111, 48);
+            this.btnRetry.TabIndex = 4;
+            this.btnRetry.Text = "재시작";
+            this.btnRetry.UseVisualStyleBackColor = false;
+            this.btnRetry.Click += new System.EventHandler(this.btnRetry_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblCount.Font = new System.Drawing.Font("한컴 윤고딕 250", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblCount.ForeColor = System.Drawing.Color.Orange;
+            this.lblCount.Location = new System.Drawing.Point(419, 271);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 28);
+            this.lblCount.TabIndex = 7;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblTimer.Font = new System.Drawing.Font("한컴 윤고딕 250", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTimer.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblTimer.Location = new System.Drawing.Point(329, 273);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 28);
+            this.lblTimer.TabIndex = 8;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(308, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "진행 시간";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(396, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "도전 횟수";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(495, 362);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnRetry);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(btnOk);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.lbl);
+            this.Font = new System.Drawing.Font("한컴 윤고딕 250", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "숫자 맞히기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRetry;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace 숫자_맞히기
+{
+    public partial class Form1 : Form
+    {
+        int computerNumber = 0;
+        int count = 0;
+
+        private int CountS = 0; //초 
+        //int a = 1;
+        private bool Toggle = false; //시작, 정지를 위한 toggle, false이면 멈춤,  true면 시간 덧셈
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl.Text = "[시작] 버튼을 클릭하세요.";
+            txt.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(1000);
+
+            lbl.Text = "1부터 100까지의 숫자를 입력하세요.";
+
+            // 게임 시작
+            Random rd = new Random();
+            computerNumber = rd.Next(1, 101); // 1 ~ 100까지의 난수 생성
+            count = 0;
+            txt.Enabled = true; // 입력을 못 하게 함. [시작]을 누르면 입력 가능
+
+            if (Toggle == false)
+            {
+                timer1.Start();
+                timer2.Start();
+                Toggle = true;
+            }
+            else 
+            {
+                timer1.Stop();
+                timer2.Stop();
+                Toggle = false;
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            
+            if (txt.Text.Trim() == "")
+            {
+                txt.Focus();
+                return;
+            }
+            int n = int.Parse(txt.Text);
+            if (n == computerNumber)
+            {
+                int c = Convert.ToInt32(lblCount.Text) + 1;
+                int t = Convert.ToInt32(lblTimer.Text); 
+                lbl.Text = "정답입니다. " + c + "번의 도전 후 " + t + "초만에 성공했습니다.";
+                timer1.Stop();
+                timer2.Stop();
+                Toggle = false;
+            }
+            else if (n < computerNumber)
+            {
+                lbl.Text = "더 큰 수를 입력하세요.";
+            }
+            else
+            {
+                lbl.Text = "더 작은 수를 입력하세요.";
+            }
+            count++;
+            if (count > 9)
+            {
+                lbl.Text = "맞히지 못 했습니다.";
+                txt.Enabled = false; // 입력을 못 하게 설정
+                timer1.Stop();
+                timer2.Stop();
+                Toggle = false;
+            }
+            lblCount.Text = count.ToString();
+        }
+
+        private void btnRetry_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // 단순 덧셈 연산
+        {
+            ++CountS;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) // 조건문을 통한 lbl의 텍스트를 바꿔줌
+        {
+            lblTimer.Text = CountS.ToString();
+        }
+    }
+}
